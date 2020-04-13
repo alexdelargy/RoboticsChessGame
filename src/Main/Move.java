efcbd8166603e4d37632516037e6ec982a379f1d
alexdelargy <addelargy97@gmail.com>
2020-04-13 15:15:38 -0400

    changes

--- src/Main/Move.java
+++ src/Main/Move.java
@@ -27,35 +27,35 @@ public class Move{
 		else return "Invalid Move";
 	}
 	public String queen(int xInit,int yInit, int xFinal, int yFinal, boolean moveType) {
-		if(KingWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
+		if(QueenWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
 			board.boardPieces[xFinal][xInit]=board.boardPieces[xInit][xFinal];
 			return "Valid Move";
 		}
 		else return "Invalid Move";
 	}
 	public String bishop(int xInit,int yInit,int xFinal, int yFinal, boolean moveType) {
-		if(KingWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
+		if(BishopWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
 			board.boardPieces[xFinal][xInit]=board.boardPieces[xInit][xFinal];
 			return "Valid Move";
 		}
 		else return "Invalid Move";
 	}
 	public String horse(int xInit, int yInit, int xFinal, int yFinal, boolean moveType) {
-		if(KingWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
+		if(HorseWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
 			board.boardPieces[xFinal][xInit]=board.boardPieces[xInit][xFinal];
 			return "Valid Move";
 		}
 		else return "Invalid Move";
 	}
 	public String rook(int xInit, int yInit, int xFinal, int yFinal, boolean moveType) {
-		if(KingWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
+		if(RookWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
 			board.boardPieces[xFinal][xInit]=board.boardPieces[xInit][xFinal];
 			return "Valid Move";
 		}
 		else return "Invalid Move";
 	}
 	public String pawn(int xInit, int yInit, int xFinal, int yFinal, boolean moveType) {
-		if(KingWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
+		if(PawnWhite.validMove(xInit,yInit,xFinal,yFinal,moveType)) {
 			board.boardPieces[xFinal][xInit]=board.boardPieces[xInit][xFinal];
 			return "Valid Move";
 		}
